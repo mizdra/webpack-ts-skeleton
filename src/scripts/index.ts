5035ca94c7d86c08ebd7d29755612b940ea9cb31
mizdra <pp.mizdra@gmail.com>
2018-01-04 00:22:00 +0900

    update tsconfig.json

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,4 +1,4 @@
 window.addEventListener("load", () => {
     const ele = document.querySelector("body");
-    ele.innerHTML = "☠ webpack-ts-skeleton ☠";
+    if (ele) ele.innerHTML = "☠ webpack-ts-skeleton ☠";
 }, true);
