539a2e4c4193059567375f87f56b3188e40cd196
mizdra <pp.mizdra@gmail.com>
2019-12-02 00:24:24 +0900

    material-ui導入

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
 export type AppProps = {};
 
 export function App(_props: AppProps) {
-  return <div>text</div>;
+  return (
+    <Container maxWidth="sm">
+      <Button variant="contained" color="primary">
+        Button
+      </Button>
+    </Container>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './app';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-render(<App />, document.querySelector('#app'));
+render(
+  <>
+    <CssBaseline />
+    <App />
+  </>,
+  document.querySelector('#app'),
+);
